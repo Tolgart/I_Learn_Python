b104b52062c448e258a919986e53e4795a514c69
Tolgart <artserge1@yandex.ru>
2026-09-06 22:40:01 +0900

    fix: solve tasks 3.11 - from 10 to 12

---
+++ Tasks/Dunder methods/len and abs/len_practice.py
@@ -0,0 +1,36 @@
+class Hero:
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __str__(self):
+        str_form = '\n'.join(f'{key}: {value}' for key, value in sorted(self.__dict__.items()))
+        return str_form
+
+
+# Ниже код для проверки методов класса Hero
+hero = Hero()
+assert len(hero) == 0
+hero.health = 50
+hero.damage = 10
+assert len(hero) == 2
+assert str(hero) == '''damage: 10
+health: 50'''
+hero.weapon = ['sword', ' bow']
+hero.skill = 'Некромант'
+assert str(hero) == '''damage: 10
+health: 50
+skill: Некромант
+weapon: ['sword', ' bow']'''
+print(hero)
+
+villain = Hero()
+assert str(villain) == ''
+assert len(villain) == 0
+villain.level = 15
+villain.skill = 'Боец'
+villain.armor = 25
+assert len(villain) == 3
+assert str(villain) == '''armor: 25
+level: 15
+skill: Боец'''
+print(villain)
